66e5a02fd34e5641a0682bbff6b27e309fba84e3
Martin Cekodhima <mcekodhima@gmail.com>
2016-11-30 12:43:27 +0100

    Added search box

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var photo = new UnsplashPhoto();
 
-    $(".background").css("background", "url(" + photo.randomize("daily").size(1920,1080).fetch() + ") no-repeat center center fixed");
+    $(".background").css("background", "url(" + photo.fromCategory("nature").size(1920,1080).fetch() + ") no-repeat center center fixed");
+    
     
 });
